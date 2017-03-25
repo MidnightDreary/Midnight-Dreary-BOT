8f517c73e7adace83e9dbc2c30d5ad2b8906b380
Nils Theres <nilsntth@gmail.com>
2017-03-25 15:02:30 +0100

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from discord.ext.commands import when_mentioned_or
 from casca.bot import Casca
 
 if __name__ == "__main__":
-    bot = Casca(command_prefix=when_mentioned_or("!"))
+    bot = Casca(command_prefix=when_mentioned_or("$"))
     bot.run()
